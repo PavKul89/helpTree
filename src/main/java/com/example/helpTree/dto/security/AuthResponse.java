ec06f81de808148326772c00b34c0dab918d53f7
Pavel Kulazhanka <PavKul89@gmail.com>
2026-03-03 16:30:12 +0300

    add DTO security and field password for User

---
+++ src/main/java/com/example/helpTree/dto/security/AuthResponse.java
@@ -0,0 +1,10 @@
+package com.example.helpTree.dto.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
