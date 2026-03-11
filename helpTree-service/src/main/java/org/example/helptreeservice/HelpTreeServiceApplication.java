15641e3361f1a5c40b796d8eccc2cf66609a9449
Pavel Kulazhanka <PavKul89@gmail.com>
2026-03-11 10:06:25 +0300

    the state of the project before starting work with the rating

--- helpTree-service/src/main/java/org/example/helptreeservice/HelpTreeServiceApplication.java
+++ helpTree-service/src/main/java/org/example/helptreeservice/HelpTreeServiceApplication.java
@@ -1,13 +1,16 @@
 package org.example.helptreeservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class HelpTreeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelpTreeServiceApplication.class, args);
+        log.info("🚀 Project started successfully at: http://localhost:8080");
     }
 
 }
