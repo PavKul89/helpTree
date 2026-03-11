471eef13e20772768d289fe7838a543143b98fc8
Pavel Kulazhanka <PavKul89@gmail.com>
2026-03-11 10:34:08 +0300

    in Redis-client replacement jedis

--- rating-service/src/main/java/org/example/ratingservice/RatingServiceApplication.java
+++ rating-service/src/main/java/org/example/ratingservice/RatingServiceApplication.java
@@ -1,13 +1,16 @@
 package org.example.ratingservice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class RatingServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RatingServiceApplication.class, args);
+        log.info("🚀 Project started successfully at: http://localhost:8082");
     }
 
 }
