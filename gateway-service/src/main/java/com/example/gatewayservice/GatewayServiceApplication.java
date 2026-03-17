8acac9111b7d53bffbda99a649d441334d05c795
Pavel Kulazhanka <PavKul89@gmail.com>
2026-03-17 12:51:59 +0300

    finished gateway-service again spring boot 4.0.3

--- gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -2,6 +2,9 @@ package com.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
@@ -10,4 +13,15 @@ public class GatewayServiceApplication {
         SpringApplication.run(GatewayServiceApplication.class, args);
     }
 
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("helpTree-service", r -> r
+                        .path("/api/**")
+                        .uri("http://localhost:8081"))
+                .route("rating-service", r -> r
+                        .path("/ratings/**")
+                        .uri("http://localhost:8085"))
+                .build();
+    }
 }
