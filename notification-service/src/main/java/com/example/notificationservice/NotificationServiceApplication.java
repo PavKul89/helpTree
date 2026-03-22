4a71578c8588bc964b0aa3a1cf97297bab572eaf
Pavel Kulazhanka <PavKul89@gmail.com>
2026-03-22 11:15:51 +0300

    create module notification

---
+++ notification-service/src/main/java/com/example/notificationservice/NotificationServiceApplication.java
@@ -0,0 +1,23 @@
+package com.example.notificationservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class NotificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+        log.info("🚀 Api-gateway service started successfully at: http://localhost:8081");
+        log.info("🚀 HelpTree service started successfully at: http://localhost:8081");
+        log.info("🚀 Rating service started successfully at: http://localhost:8085");
+        log.info("🚀 Kafka-ui successfully at: http://localhost:8082");
+        log.info("🚀 Prometheus successfully at: http://localhost:9090");
+        log.info("🚀 Grafana successfully at: http://localhost:3000");
+        log.info("🚀 Jaeger successfully at: http://localhost:16686");
+        log.info("🚀 MinIO successfully at: http://localhost:9001/browser/helptree");
+    }
+
+}
